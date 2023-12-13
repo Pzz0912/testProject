34270b401ff9c21aa768c63493532c6bf76e2da6
Pzz0912 <pzz@onyx-international.com>
2023-12-13 16:20:12 +0800

    Add release workflow

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
-import { Editor, Plugin } from "obsidian";
+import { Editor, Plugin, Menu, Notice } from "obsidian";
 import { InsertLinkModal } from "./src/modal";
 
 export default class InsertLinkPlugin extends Plugin {
 	async onload() {
+		this.setMenu();
 		this.addCommand({
 			id: "insert-link",
 			name: "Insert link",
@@ -14,6 +15,43 @@ export default class InsertLinkPlugin extends Plugin {
 
 				new InsertLinkModal(this.app, selectedText, onSubmit).open();
 			},
+		});
+	}
+
+	onunload() {
+		console.log("unloading plugin");
+	}
+
+	//
+	setMenu() {
+		this.addRibbonIcon("dice", "Open menu", (event) => {
+			const menu = new Menu();
+			menu.addItem((item) =>
+				item
+					.setTitle("Copy")
+					.setIcon("documents")
+					.onClick(() => {
+						new Notice("Copied");
+						const selectedText =
+							this.app?.workspace?.activeEditor.getSelection();
+						navigator.clipboard.writeText(selectedText);
+					})
+			);
+
+			menu.addItem((item) =>
+				item
+					.setTitle("Paste")
+					.setIcon("paste")
+					.onClick(async () => {
+						new Notice("Pasted");
+						const text = await navigator.clipboard.readText();
+						this.app?.workspace?.activeLeaf?.view?.editor?.replaceSelection(
+							text
+						);
+					})
+			);
+
+			menu.showAtMouseEvent(event);
 		});
 	}
 }
